8cee43d7e7a9be6b4b9881b622f82edcffc8873c
Jere Benítez <jerebntz@gmail.com>
2023-12-17 09:10:28 -0300

    added polybar config

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,6 +6,6 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+THEME="rosepine"
 # Launch polybar
-polybar main -c ~/.config/polybar/config &
-# polybar secondary -c ~/.config/polybar/config &
+polybar main -c ~/.config/polybar/themes/$THEME/config.ini &
